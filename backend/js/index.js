5f056ea95a352904aa69ddf6be02658d738391b1
Wei Sheng Ang <aws1@hotmail.sg>
2023-04-02 19:48:05 +0800

    charity accept listing

--- backend/js/index.js
+++ backend/js/index.js
@@ -16,7 +16,7 @@ const auth = app.auth();
 
 $(document).ready(function () {
     $(".newListingContainer").hide()
-    
+
 
     // Listen for authentication state changes
     auth.onAuthStateChanged((user) => {
@@ -44,7 +44,7 @@ function retrieveUserType(userid) {
                     if (result.data.userType == "bakery") {
                         $(".newListingContainer").show()
                     }
-                    showListings(result.data.userType)
+                    showListings(result.data.userType, userid)
                 }
             }
         } catch (error) {
@@ -54,9 +54,15 @@ function retrieveUserType(userid) {
 }
 
 
-function showListings(info) {
+function showListings(userType, userid) {
     $(async () => {
-        var serviceUrl = "http://localhost:5004/listings"
+        if (userType == "bakery") {
+            var serviceUrl = "http://localhost:5004/listings/" + userType + "/" + userid
+        } else {
+            var serviceUrl = "http://localhost:5004/listings"
+        }
+
+
         try {
             const response = await fetch(serviceUrl, {
                 method: "GET"
@@ -65,7 +71,7 @@ function showListings(info) {
             if (response.ok) {
                 if (response.status === 200) {
                     console.log(result.data)
-                    if (info == "bakery") {
+                    if (userType == "bakery") {
                         for (listing of result.data) {
 
                             //allergens need to fix
@@ -83,9 +89,10 @@ function showListings(info) {
 
                         }
 
-                    } else if (info == "charity") {
+                    } else if (userType == "charity") {
+                        var count = 1;
                         for (listing of result.data) {
-                            if (["created", "picking up", "delivering", "delivered"].includes(listing.status)) {
+                            if (["picking up", "delivering", "delivered"].includes(listing.status) || (listing.status == "accepted" && listing.charityId == userid)) {
                                 //allergens need to fix
                                 $("#listings").append(`
                                 <tr>
@@ -93,12 +100,29 @@ function showListings(info) {
                                     <td class="align-middle">${listing.breadContent}</td>
                                     <td class="align-middle">${listing.releaseTime}</td>
                                     <td class="align-middle">${listing.allergens}</td>
-
+                                    <td class="align-middle"><button type="button" class="btn btn-secondary disabled">${listing.status}</button></td>
                                 </tr>
                             `)
-                            }
 
-                        }
+                            } else if (listing.status == "created") {
+                                { 
+                                    //allergens need to fix
+                                    
+                                    $("#listings").append(`
+                                    <tr>
+                                        <td class="align-middle">${listing.bakeryName}</td>
+                                        <td class="align-middle">${listing.breadContent}</td>
+                                        <td class="align-middle">${listing.releaseTime}</td>
+                                        <td class="align-middle">${listing.allergens}</td>
+                                        <td class="align-middle"><button type="button" class="btn btn-primary" onclick="acceptOrder(this.id)" id=${listing.id}>accept</button></td>
+                                    </tr>
+                                `)
+    
+                                }
+                            }
+                        count++;
+                        } 
+                        
                     } else if (info == "volunteer") {
                         for (listing of result.data) {
                             if (["accepted", "picking up", "delivering", "delivered"].includes(listing.status)) {
@@ -155,6 +179,7 @@ function addListing() {
                 breadContent: parseInt($("#breadContent").val()),
                 charityId: "",
                 charityName: "",
+                status: "created"
                 //createTime: firestore.Timestamp.now()
             })
 
@@ -181,3 +206,94 @@ function addListing() {
     });
 }
 //filter by usertype
+
+// function acceptOrder(userid) {
+//     console.log(userid)
+//     // document.getElementById("acceptButton"+count).innerHTML = `<button type="button" class="btn btn-secondary disabled">Accepted</button>`
+
+// }
+function acceptOrder(listingId) {
+    var id = listingId
+
+    document.getElementById(id).className = "btn btn-secondary disabled";
+    document.getElementById(id).innerHTML = "accepted";
+
+
+    auth.onAuthStateChanged(async (user) => {
+        if (user) {
+
+
+            var serviceUrl = "http://localhost:5002/charities/" + user.uid
+            const response = await fetch(serviceUrl, {
+                method: "GET"
+            })
+            const result = await response.json()
+            var charityName = result.data.name
+
+            var serviceUrl = "http://localhost:5004/listings/" + id 
+
+            data = JSON.stringify({
+                charityId: user.uid,
+                charityName: charityName,
+                status: "accepted"
+                //createTime: firestore.Timestamp.now()
+            })
+
+            try {
+                const response = await fetch(serviceUrl, {
+                    headers: {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json"
+                    },
+                    method: "PUT",
+                    body: data
+                })
+                const result = await response.json()
+                if (response.ok) {
+                    if (response.status == 201) {
+                        alert("Accepted")
+                        retrieveUserType(user.uid)
+                    }
+                }
+            } catch (error) {
+                alert("Error creating report.")
+                alert(error.message)
+            }
+        }
+    });
+}
+
+function updateStatus(listingid, status, uid) {
+    $(async () => {
+        var serviceUrl = "http://localhost:5004/listings/" + listingid
+        if (status == "pickedup") {
+            var data = JSON.stringify({
+                status: "delivering"
+            })
+        }
+        else {
+            var data = JSON.stringify({
+                status: "delivered"
+            })
+        }
+        try {
+            const response = await fetch(serviceUrl, {
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                method: "PUT",
+                body: data
+            })
+            const result = await response.json()
+            if (response.ok) {
+                if (response.status == 200) {
+                    alert("Listing updated.")
+                
+                }
+            }
+        } catch (error) {
+            alert("Error updating bakery.")
+        }
+    })
+}
