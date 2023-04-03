599d8d296d1aa60eb2a765bfbe2d1c556d1a6267
Chester Chia <chester.chia@gmail.com>
2023-04-03 16:41:30 +0800

    update updatereport

--- backend/routes/update_report.py
+++ backend/routes/update_report.py
@@ -4,18 +4,22 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route("/updateReport/<string:reportId>/<string:isBanned>", methods=["PUT"])
-def updateReport(reportId, isBanned):
+@app.route("/updateReport/<string:reportId>/<string:isBanned>/<string:userType>", methods=["PUT"])
+def updateReport(reportId, isBanned, userType):
     reportData = requests.get("http://127.0.0.1:5004/listings/" + reportId)
     report = reportData.json()["data"]
     listingId = report["listingId"]
     listingData = requests.get("http://127.0.0.1:5004/listings/" + listingId)
     listing = listingData.json()["data"]
-    bakeryId = listing["bakeryId"]
     data = {
-        "isBanned": isBanned
-    }
-    requests.put("http://127.0.0.1:5001/bakeries/" + bakeryId, data=data)
+            "isBanned": isBanned
+        }
+    if userType == "bakery":
+        bakeryId = listing["bakeryId"]
+        requests.put("http://127.0.0.1:5001/bakeries/" + bakeryId, data=data)
+    else:
+        volunteerId = listing["volunteerId"]
+        requests.put("http://127.0.0.1:5003/volunteers/" + volunteerId, data=data)
 
 if __name__ == '__main__':
     app.run(port=5009, debug=True)
